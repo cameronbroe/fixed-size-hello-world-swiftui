1734f614a1ec1e89cad3f1c3eb30c304a728551e
Cameron Roe <croe@macstadium.com>
2023-11-02 12:52:24 -0500

    Initial Commit

---
+++ FixedSize/FixedSizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixedSizeApp.swift
+//  FixedSize
+//
+//  Created by Cameron Roe on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FixedSizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
